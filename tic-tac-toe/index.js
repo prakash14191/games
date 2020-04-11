715a69e3590279ba655e0589895eec6150bb2044
TummanapallyAnuraag <tummanapally.anuraag@gmail.com>
2020-04-11 21:04:47 +0530

    tic-tac-toe win scenario updated

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,7 +1,7 @@
 // even X, odd O
 window.turn = 0;
-window.puli_cap = 3;
-window.meka_cap = 3;
+window.puli_cap = 9;
+window.meka_cap = 9;
 window.playground = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
 var player = document.getElementById("player");
@@ -41,18 +41,23 @@ function draw_grid(ctx){
     ctx.moveTo(0, 2*h/3);
     ctx.lineTo(w, 2*h/3);
 
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#0da192';
     ctx.stroke();
 }
 
 function put_puli(context, x, y){
+    if(window.playground[ pos_to_index(y) ][ pos_to_index(x) ] != ' '){
+        alert('Illegal Move');
+        return 0;
+    }
     puli_img = new Image();
     puli_img.src = 'puli.png';
     puli_img.onload = function(){
         context.drawImage(puli_img, x-32, y-32, 75, 75);
-        window.puli_cap--;
-        window.playground[ pos_to_index(y) ][ pos_to_index(x) ] = 'P';
     }
+    window.puli_cap--;
+    window.playground[ pos_to_index(y) ][ pos_to_index(x) ] = 'P';
     return puli_img;
 }
 
@@ -68,13 +73,17 @@ function remove_puli(context, x, y){
 }
 
 function put_meka(context, x, y){
+    if(window.playground[ pos_to_index(y) ][ pos_to_index(x) ] != ' '){
+        alert('Illegal Move');
+        return 0;
+    }
     meka_img = new Image();
     meka_img.src = 'meka.png';
     meka_img.onload = function(){
         context.drawImage(meka_img, x-32, y-32, 75, 75);
-        window.meka_cap--;
-        window.playground[ pos_to_index(y) ][ pos_to_index(x) ] = 'M';
     }
+    window.meka_cap--;
+    window.playground[ pos_to_index(y) ][ pos_to_index(x) ] = 'M';
     return meka_img;
 }
 
@@ -90,6 +99,10 @@ function remove_meka(context, x, y){
 }
 
 canvas.addEventListener("mousedown", function(e){
+    if(window.turn > 9){
+        alert('All positions filled!');
+        return;
+    }
     [x,y] = getMousePosition(canvas, e);
     /* 20px padding */
     x = adjust_position(x - 20);
@@ -97,22 +110,32 @@ canvas.addEventListener("mousedown", function(e){
 
     if(window.turn %2 == 0){
         if(window.puli_cap > 0){
-            put_puli(ctx, x, y);
-            window.turn++;
-            player.innerHTML = '<img src="meka.png" width="25" /> Meka';
+            if(put_puli(ctx, x, y)){
+                window.turn++;
+                player.innerHTML = '<img src="meka.png" width="25" /> Meka';
+            }
         }else{
             remove_puli(ctx, x, y);
         }
     }else{
         if(window.meka_cap > 0){
-            put_meka(ctx, x, y);
-            window.turn++;
-            player.innerHTML = '<img src="puli.png" width="25" /> Puli';
+            if( put_meka(ctx, x, y) ){
+                window.turn++;
+                player.innerHTML = '<img src="puli.png" width="25" /> Puli';
+            }
         }else{
             remove_meka(ctx, x, y);
         }
     }
-    
+    if( check_win_scenario() ){
+        alert('Hurray!');
+        return;
+    }else{
+        if(window.turn == 9){
+            alert('Draw!');
+            return;
+        }
+    }
 });
 
 function getMousePosition(canvas, event) { 
@@ -135,4 +158,30 @@ function adjust_position(pos){
 
 function pos_to_index(pos){
     return parseInt( (pos - 50)/100 );
+}
+
+function check_win_scenario(){
+    var a = window.playground;
+    console.log(a);
+    // check for row
+    for(i = 0; i< 3; i++){
+        if(a[i][1] == a[i][0] && a[i][2] == a[i][0] && a[i][0] != " "){
+            return 1;
+        }
+    }
+    // check for column
+    for(i = 0; i< 3; i++){
+        if(a[1][i] == a[0][i] && a[2][i] == a[0][i] && a[0][i] != ' '){
+            return 1;
+        }
+    }
+    // diagonal
+    if(a[0][0] == a[1][1] && a[2][2] == a[1][1] && a[1][1] != ' '){
+        return 1;
+    }
+    // diagonal
+    if(a[0][2] == a[1][1] && a[2][0] == a[1][1] && a[1][1] != ' '){
+        return 1;
+    }
+    return 0;
 }
